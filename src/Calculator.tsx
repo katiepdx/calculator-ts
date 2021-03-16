1e04026c0fd79676c3385ef6e9ff86455cb8ce2f
katiePDX <61799774+katiepdx@users.noreply.github.com>
2021-03-16 10:49:25 -0700

    add ts calc functions

---
+++ src/Calculator.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { calculation } from './math-utils.js'
+
+interface CalculatorProps {
+  symbol: string,
+  buttonName: string
+}
+
+const Calculator = ({ symbol, buttonName }: CalculatorProps) => {
+  const [num1, setNum1] = useState(null);
+  const [num2, setNum2] = useState(null);
+  const [answer, setAnswer] = useState(0)
+
+  const handleNum1Change = (e: any) => {
+    setNum1(e.target.value)
+  }
+  const handleNum2Change = (e: any) => {
+    setNum2(e.target.value)
+  }
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    if (num1 === null || num2 === null) alert('Please enter a number')
+
+    const total = calculation(symbol, num1, num2)
+    setAnswer(total)
+  }
+  return (
+    <div>
+      <label htmlFor="number1">
+        <input type="number" name="number1" onChange={handleNum1Change} />
+      </label>
+      {symbol}
+      <label htmlFor="number2">
+        <input type="number" name="number2" onChange={handleNum2Change} />
+      </label>
+
+      <div>{answer}</div>
+      <button onClick={handleClick}>{buttonName}</button>
+    </div>
+  )
+}
+
+export default Calculator
